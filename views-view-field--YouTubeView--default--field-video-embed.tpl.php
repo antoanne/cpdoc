bfa8783d3934ae3ef284135fef4da61412983c65
Antoanne Pontes <antoanne@gmail.com>
2009-10-11 07:25:01 -0300

    Ajustes para IE usar lightbox2

--- views-view-field--YouTubeView--default--field-video-embed.tpl.php
+++ views-view-field--YouTubeView--default--field-video-embed.tpl.php
@@ -1,9 +1,8 @@
 <div id="YouTubeFeedTumb">
 <?php
-   //    if (isset($_SERVER['HTTP_USER_AGENT']) && 
-   // (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false))
-   //     print str_replace("lightframe[youtubefeed|width:441; height:366;]", "lightframe[|width:441; height:366;]", $output);
-   // else
-        print $output;
+   if (isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false))
+     print str_replace("lightframe[youtube_feed|width:441; height:366;]", "lightframe[|width:461; height:386;]", $output);
+   else
+     print $output;
 ?>
 </div>
